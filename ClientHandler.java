136ce4f9faefb44bb80ed1404836d99af1615e43
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-07-15 23:10:16 -0700

    unused imports

--- ClientHandler.java
+++ ClientHandler.java
@@ -3,11 +3,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
 	private Socket client;
 	private BufferedReader br;
 	private BufferedWriter bw;
--- Server.java
+++ Server.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
